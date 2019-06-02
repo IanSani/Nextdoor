45a725645fd3693265c0854c54079408ff3c84c7
IanSani <iansani259@gmail.com>
2019-06-02 21:52:28 +0300

    Created class location to provide location functionality

--- mtaa/models.py
+++ mtaa/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=100,null=True)
+
+    def __str__(self):
+        return self.name
