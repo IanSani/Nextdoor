f4e98fe922f3b1808438a3b0e39030817afc3e3e
IanSani <iansani259@gmail.com>
2019-06-02 22:24:50 +0300

    REgistered models

--- mtaa/admin.py
+++ mtaa/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Hood)
+admin.site.register(Business)
+admin.site.register(Location)
