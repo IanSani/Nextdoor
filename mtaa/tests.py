6715916827c89646b510fa3f2cceec94871544b0
IanSani <iansani259@gmail.com>
2019-06-02 22:31:48 +0300

    Created buysiness test class to chek if users could add businesses in hoods

--- mtaa/tests.py
+++ mtaa/tests.py
@@ -34,3 +34,36 @@ class HoodTest(TestCase):
         self.south.save()
         hoods = Hood.get_hoods()
         self.assertTrue(len(hoods) > 0)
+
+
+class BusinessTest(TestCase):
+    def setUp(self):
+        self.mpesa= Business.objects.create(b_name='star',b_description='dope',b_email='xyz.test.com')
+
+    def test_instance(self):
+        self.mpesa.save()
+        self.assertTrue(isinstance(self.mpesa,Business))
+
+    def test_get_business(self):
+        self.mpesa.save()
+        business = Business.get_business()
+        self.assertTrue(len(business) >0 )
+
+class PostsTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id = 1, username='zyzu')
+        self.Kinoo = Location.objects.create(name='Kinoo')
+
+        self.south = Hood.objects.create(
+            hood_name='south',occupants_count =1, location=self.Kinoo)
+
+        self.security= Posts.objects.create(title='shida',content='soja ko doze manze',posted_by= self.user, hood= self.south)
+
+    def test_instance(self):
+        self.security.save()
+        self.assertTrue(isinstance(self.security,Posts))
+
+    def test_delete_posts(self):
+        self.security.save()
+        self.security.delete()
+        self.assertTrue(len(Posts.objects.all()) == 0)
