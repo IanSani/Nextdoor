41b5bdccde8e2ce788fccdf3691a6b6c9ea5c950
IanSani <iansani259@gmail.com>
2019-06-02 21:53:12 +0300

    Created class hood to allow users enter their hood

--- mtaa/models.py
+++ mtaa/models.py
@@ -9,3 +9,23 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+
+class Hood(models.Model):
+    hood_photo = models.ImageField(upload_to='hoods/')
+    hood_name = models.CharField(max_length=100, null=True)
+    occupants_count = models.PositiveIntegerField(default=0)
+    location = models.ForeignKey(Location, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    @classmethod
+    def get_hoods(cls):
+        hoods = Hood.objects.all()
+        return hoods
+
+    @classmethod
+    def search_hood(cls,hood_search):
+        hoods = cls.objects.filter(id__icontains = hood_search)
+        return hoods
+
+    class Meta:
+        ordering = ['hood_name']
