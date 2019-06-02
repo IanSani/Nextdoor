2c620153791471b6336457f6ec06fdf4747c8e27
IanSani <iansani259@gmail.com>
2019-06-02 22:02:05 +0300

    CReated function to enable creation of profiles

--- mtaa/views.py
+++ mtaa/views.py
@@ -31,3 +31,27 @@ def home(request):
     else:
         neighbourhoods = Hood.objects.all()
         return render(request, 'index.html', {"neighbourhoods": neighbourhoods})
+
+
+def new_business(request):
+    current_user = request.user
+
+    if request.method == 'POST':
+        form = BusinessForm(request.POST, request.FILES)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.user = current_user
+            business.hood = request.user.join.hood_id
+            business.save()
+            return redirect('home')
+
+    else:
+        form = BusinessForm()
+    return render(request, 'business.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    profile = Profile.objects.get(user=request.user)
+    hoods = Hood.objects.filter(user=request.user).all()
+    business = Business.objects.filter(user=request.user).all()
+    return render(request, 'profiles/profile.html', {"profile": profile, "hoods": hoods, "business": business})
