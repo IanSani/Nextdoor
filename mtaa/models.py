95a512c40dda99c4d393d26ad82bbe4f0b082980
IanSani <iansani259@gmail.com>
2019-06-02 21:54:52 +0300

    Created class profile to enable users create profiles

--- mtaa/models.py
+++ mtaa/models.py
@@ -42,3 +42,29 @@ class Business(models.Model):
     def get_business(cls):
         business = Business.objects.all()
         return business
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    profile_photo= models.ImageField(upload_to='profiles/',null=True)
+    bio= models.CharField(max_length=240, null=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+        post_save.connect(create_user_profile, sender=User)
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+    @classmethod
+    def get_profile(cls):
+        profile = Profile.objects.all()
+        return profile
+
+    class Meta:
+        ordering = ['user']
