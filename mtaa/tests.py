1faacbccbe48497737781a6640ac2739151b789f
IanSani <iansani259@gmail.com>
2019-06-02 22:29:43 +0300

    Created profile test to check if profile was functioning

--- mtaa/tests.py
+++ mtaa/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from .models import *
 # Create your tests here.
+
+class ProfileTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(id =1, username='zyzu')
+        self.profile = Profile.objects.create(user = self.user,bio = 'blow away',email='abc@test.com')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    def test_save_profile(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    def test_get_profile(self):
+        self.profile.save()
+        profile = Profile.get_profile()
+        self.assertTrue(len(profile) > 0)
